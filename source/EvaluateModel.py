193c7eeca06ac235a12360f43ad08e379255640d
Daniel <daniel00150015@gmail.com>
2023-12-13 22:52:01 +0100

    Added wider range on test plot

--- source/EvaluateModel.py
+++ source/EvaluateModel.py
@@ -6,24 +6,18 @@ import numpy as np
 import pandas as pd
 
 class EvaluteModel():
-    def __init__(self, X_test: pd.DataFrame, y_test: pd.DataFrame, toTest: pd.DataFrame, features: [str], county_names, random_state=None) -> None:
+    def __init__(self, X_test: pd.DataFrame, y_test: pd.DataFrame, X_validation: pd.DataFrame, county_names, random_state=None) -> None:
         self.X_test = X_test
         self.y_test = y_test
+        self.X_validation = X_validation
         self.county_names = county_names
-        self.toTest = toTest
-        self.features = features
-
-        self.__createTest(random_state)
+        self.random_day = random_state
 
     def test(self, model, random_day=None):
         self.results = pd.DataFrame()
         self.resultsPerCounty = pd.DataFrame(columns=['County', 'MAE', 'MSE'])
 
-        if random_day is not None:
-            self.__createTest(random_day)
-
         self.y_pred = model.predict(self.X_test)
-        self.y_val = model.predict(self.validationTest)
 
         self.__MAE()
         self.__MSE()
@@ -31,21 +25,7 @@ class EvaluteModel():
         display(self.results)
         display(self.resultsPerCounty)
 
-        self.__Plot()
-
-    def __createTest(self, random_day = None, random_state=None):
-        # Create practical test from data
-        if random_day is not None:
-            self.random_day = random_day
-        else:
-            self.random_day = self.toTest['datetime_date'].sample(random_state=random_state).iloc[0]
-
-        self.validationData = self.toTest[
-            (self.toTest.datetime_date == self.random_day)
-        ]
-
-        self.validationAnswers = self.validationData['target']
-        self.validationTest = self.validationData[self.features]
+        self.__Plot(random_day)
     
     def __metricsPerCounty(self):
         validationData = self.X_test[['county']].copy()
@@ -68,9 +48,26 @@ class EvaluteModel():
     def __MSE(self):
         self.results['MSE'] = [MSE(self.y_pred, self.y_test)]
 
-    def __Plot(self):
-        validationData = self.validationData[['datetime', 'target', 'is_consumption', 'is_business']].copy()
-        validationData['predictions'] = self.y_val.tolist()
+    def __Plot(self, random_day = None):
+
+        # Get random date for plot
+        if random_day is not None:
+            self.random_day = random_day
+        else:
+            self.random_day = self.X_validation['datetime_date'].sample(random_state=self.random_state).iloc[0]
+
+        # Get week based on random date
+        self.X_validation['target'] = self.y_test
+        self.X_validation['predictions'] = self.y_pred.tolist()
+        self.validationData = self.X_validation[
+            (self.X_validation.datetime_date >= self.random_day - datetime.timedelta(days=3)) &
+            (self.X_validation.datetime_date <= self.random_day + datetime.timedelta(days=3))
+        ]
+
+        if len(self.validationData) == 0:
+            display(f'Warning: Test data in range {min(self.X_validation.datetime_date)} - {max(self.X_validation.datetime_date)}')
+
+        validationData = self.validationData[['datetime', 'target', 'is_consumption', 'is_business', 'predictions']].copy()
         validationData = validationData.groupby(by=['datetime', 'is_consumption', 'is_business']).mean().reset_index()
 
         fig = px.line(validationData, x='datetime', y=['target', 'predictions'], facet_col='is_consumption', facet_row='is_business', title=f'Example')
